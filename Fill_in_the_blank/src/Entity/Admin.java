3c9fdb83e6406fe7cc4ce9c46968c31475e0ebc8
ChoonPengKeng <68885754+ChoonPengKeng@users.noreply.github.com>
2020-08-24 10:50:59 +0800

    delete some comment

--- Fill_in_the_blank/src/Entity/Admin.java
+++ Fill_in_the_blank/src/Entity/Admin.java
@@ -68,52 +68,42 @@ public class Admin{
     public boolean validateName(String name) 
     { 
   
-        // Regex to check valid username. 
+     
         String regex = "^[aA-zZ]\\w{2,29}$"; 
   
-        // Compile the ReGex 
+
         Pattern p = Pattern.compile(regex); 
          
-        // If the username is empty 
-        // return false 
+
         if (name == null) { 
             return false; 
         } 
   
-        // Pattern class contains matcher() method 
-        // to find matching between given username 
-        // and regular expression. 
+
         Matcher m = p.matcher(name); 
-  
-        // Return if the username 
-        // matched the ReGex 
+
         return m.matches(); 
     } 
     
     public boolean isValidPassword(String password) 
     { 
   
-        // Regex to check valid password. 
+ 
         String regex = "^(?=.*[0-9])"
                        + "(?=.*[a-z])(?=.*[A-Z])"
                        + "(?=\\S+$).{5,20}$"; 
   
-        // Compile the ReGex 
+
         Pattern p = Pattern.compile(regex); 
   
-        // If the password is empty 
-        // return false 
+
         if (password == null) { 
             return false; 
         } 
   
-        // Pattern class contains matcher() method 
-        // to find matching between given password 
-        // and regular expression. 
+
         Matcher m = p.matcher(password); 
-  
-        // Return if the password 
-        // matched the ReGex 
+
         return m.matches(); 
     }
     
